934fc62a129eda5a20b9bf366919949dea42fe1a
marcelo <mhbcoelho99@gmail.com>
2023-10-28 16:39:01 -0300

    adding binary_code_example.txt

--- src/binary_code_generate.py
+++ src/binary_code_generate.py
@@ -5,7 +5,7 @@ linha_tamanho = 200
 num_linhas = 200
 
 # Gere um arquivo binário aleatório
-with open("codigo_binario.txt", "wb") as arquivo:
+with open("binary_code.txt", "wb") as arquivo:
     for _ in range(num_linhas):
         linha_binaria = "".join(random.choice("01") for _ in range(linha_tamanho))
         linha_bytes = bytes([int(linha_binaria[i:i+8], 2) for i in range(0, len(linha_binaria), 8)])
